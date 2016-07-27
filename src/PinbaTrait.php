11041319a24cfd48a692baf60d3b6e16330d107e
l80 <unit2013@hmamail.com>
2016-07-27 17:20:44 +0300

    PIX-202

--- src/PinbaTrait.php
+++ src/PinbaTrait.php
@@ -1,35 +1,32 @@
 <?php
-/* */
+
 namespace PinbaTrait;
 
 trait PinbaTrait {
 
-      static protected $enabled = false;
-          static protected $type;
+    static protected $enabled = false;
+    static protected $type;
 
-          static public function setType($type) {
-                    self::$type = $type;
-                        }
+    static public function setType($type) {
+        self::$type = $type;
+    }
 
-          static public function init($settings) {
-                        error_log('PinbaTrait# init');
-                        self::$enabled = function_exists('pinba_timer_start') && function_exists('pinba_timer_start');
-                                self::setType(!empty($settings['type']) ? $settings['type'] : "pinba.default.type");
-                            }
+    static public function init($settings) {
+        self::$enabled = function_exists('pinba_timer_start') && function_exists('pinba_timer_start');
+        self::setType(!empty($settings['type']) ? $settings['type'] : "pinba.default.type");
+    }
 
-              static public function pinba_timer_start($action, $type = null) {
-               error_log("pinba_time_start");
-                if(self::$enabled) {
-                                      return pinba_timer_start(['type'=> $type ?: self::$type,'action'=> $action]);
-                                              }
+    static public function pinba_timer_start($action, $type = null) {
+        if(self::$enabled) {
+            return pinba_timer_start(['type'=> $type ?: self::$type,'action'=> $action]);
+        }
 
-                                return null;
-                            }
+        return null;
+    }
 
-          static public function pinba_timer_stop($pinba) {
-            error_log("pinba_time_stop");
-                        if($pinba && self::$enabled) {
-                                      pinba_timer_stop($pinba);
-                                              }
-                            }
+    static public function pinba_timer_stop($pinba) {
+        if($pinba && self::$enabled) {
+            pinba_timer_stop($pinba);
+        }
+    }
 }
